f8fc42706fbae1cf0f7e31d745930e4f933273ea
Matt Blanco <mattmblanco@gmail.com>
2024-07-16 14:57:29 +0200

    Comment gyroscope test out

--- sketch.js
+++ sketch.js
@@ -1,33 +1,48 @@
-let theta1=0
-let theta2=0
-let theta3=0
+// let theta1 = 0
+// let theta2 = 0
+// let theta3 = 0
 
-let dthetaXdt=0;
-let dthetaYdt=0;
-let dthetaZdt=0;
+// let dthetaXdt = 0;
+// let dthetaYdt = 0;
+// let dthetaZdt = 0;
 
-let dtheta1dt=0;
-let dtheta2dt=0;
-let dtheta3dt=0;
+// let dtheta1dt = 0;
+// let dtheta2dt = 0;
+// let dtheta3dt = 0;
 
-let thetaX=0;
-let thetaY=0;
-let thetaZ=0;
+// let thetaX = 0;
+// let thetaY = 0;
+// let thetaZ = 0;
 
-let det=false
+// let det = false
 
-let t=0,dt=1;
+// let t= 0
+// let dt = 1;
 
-function cb(event){
-  dthetaXdt=event.rotationRate.alpha*PI/180;
-  dthetaYdt=event.rotationRate.beta*PI/180;
-  dthetaZdt=event.rotationRate.gamma*PI/180;
+// function cb(event){
+//   dthetaXdt= event.rotationRate.alpha * PI / 180;
+//   dthetaYdt= event.rotationRate.beta * PI / 180;
+//   dthetaZdt= event.rotationRate.gamma * PI / 180;
   
-  let ct=millis()/1000;
-  dt=ct-t;
-  t=ct;
+//   let ct = millis()/1000;
+//   dt= ct - t;
+//   t= ct;
 
-  det=true;
+//   det = true;
+// }
+
+const width = 800;
+const height = 800;
+
+let x = width / 2;
+let y = height / 2;
+
+function handleMotionEvent(event) {
+  x = event.accelerationIncludingGravity.x;
+  y = event.accelerationIncludingGravity.y;
+  const z = event.accelerationIncludingGravity.z;
+
+  // Do something awesome.
 }
 
 function onClick(){
@@ -44,87 +59,98 @@ function onClick(){
     window.addEventListener('devicemotion', cb);
     // handle regular non iOS 13+ devices
   }
+
+  document.getElementById("motion-button").style.display = "none";
 }
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(800, 800);
 }
 
-function draw(){
+function draw() {
+  // backgroun(255);
+
+  fill(211, 174, 42);
+  ellipse(x, y, 20, 20);
+
+}
+
+// function setup() {
+//   createCanvas(800, 800, WEBGL);
+// }
+
+// function draw(){
  
-  background(0);
+//   background(0);
   
-  directionalLight(255,255,255,-1,-1,-1);
-  ambientLight(80);
+//   directionalLight(255,255,255,-1,-1,-1);
+//   ambientLight(80);
 
-  dtheta1dt=dthetaXdt;
-  dtheta2dt=dthetaYdt*sin(thetaX)+dthetaZdt*cos(thetaX);
-  dtheta3dt=-dthetaYdt*cos(thetaX)*cos(theta2)
-    +dthetaZdt*sin(thetaX)*cos(theta2)
+//   dtheta1dt = dthetaXdt;
+//   dtheta2dt = dthetaYdt * sin(thetaX) + dthetaZdt * cos(thetaX);
+//   dtheta3dt =- dthetaYdt * cos(thetaX) * cos(theta2) + dthetaZdt * sin(thetaX) * cos(theta2)
   
-
-  scale(1.5)
-
-  
-  stroke(0)
+//   stroke(0);
 
 
-  let r1=100,r2=80,r3=60;
+//   let r1 = 100;
+//   let r2 = 80;
+//   let r3 = 60;
 
-  noStroke()
-  let detailX=48*2;
-  let detailY=32;
+//   noStroke()
+//   let detailX = 48 * 2;
+//   let detailY = 32;
 
-  //outer ring
-  torus(r1,5,detailX,detailY)
+//   //outer ring
+//   torus(r1, 5, detailX, detailY)
 
-  push()
-  translate((r1+r2)/2,0,0)
-  rotateZ(PI/2)
-  cylinder(5,20)
-  pop()
-  push()
-  translate(-(r1+r2)/2,0,0)
-  rotateZ(PI/2)
-  cylinder(5,20)
-  pop()
+//   push()
+//   translate((r1 + r2) / 2, 0, 0)
+//   rotateZ(PI / 2)
+//   cylinder(5, 20)
+//   pop()
+//   push()
+//   translate(-(r1 + r2) / 2, 0, 0)
+//   rotateZ(PI / 2)
+//   cylinder(5, 20)
+//   pop()
 
-  //middle ring
-  rotateX(theta1)
-  rotateX(PI/2)
-  torus(80,5,detailX,detailY)
+//   //middle ring
+//   rotateX(theta1)
+//   rotateX(PI/2)
+//   torus(80,5,detailX,detailY)
 
-  push()
-  translate(0,(r2+r3)/2,0)
-  rotateY(PI/2)
-  cylinder(5,20)
-  pop()
+//   push()
+//   translate(0,(r2+r3)/2,0)
+//   rotateY(PI/2)
+//   cylinder(5,20)
+//   pop()
 
-  push()
-  translate(0,-(r2+r3)/2,0)
-  rotateY(PI/2)
-  cylinder(5,20)
-  pop()
+//   push()
+//   translate(0,-(r2+r3)/2,0)
+//   rotateY(PI/2)
+//   cylinder(5,20)
+//   pop()
 
 
-  //inner ring
-  rotateY(theta2);
-  rotateY(PI/2)
-  torus(r3,5,detailX,detailY)
+//   //inner ring
+//   rotateY(theta2);
+//   rotateY(PI/2)
+//   torus(r3 ,5 , detailX, detailY)
 
-  //box
-  rotateX(theta3);
-  box(50)
-  rotateZ(PI/2)
-  cylinder(5,2*r3)
+//   //box
+//   rotateX(theta3);
+//   box(50)
+//   rotateZ(PI/2)
+//   cylinder(5, 2 * r3)
 
-  if(det && dtheta1dt==dtheta1dt){
-    theta1+=dtheta1dt*dt;
-    theta2+=dtheta2dt*dt;
-    theta3+=dtheta3dt*dt;
+//   if(det && dtheta1dt == dtheta1dt){
+//     theta1 += dtheta1dt * dt;
+//     theta2 += dtheta2dt * dt;
+//     theta3 += dtheta3dt * dt;
     
-    thetaX+=dthetaXdt*dt;
-    thetaY+=dthetaYdt*dt;
-    thetaZ+=dthetaZdt*dt;
-  }
-}
+//     thetaX += dthetaXdt * dt;
+//     thetaY += dthetaYdt * dt;
+//     thetaZ += dthetaZdt * dt;
+//   }
+// }
